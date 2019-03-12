1c81e3a7d11990ec9997f98af7f3a5561906a431
sbrink00 <sbrink00@stuy.edu>
2019-03-12 06:07:00 -0400

    working on for loop in partition

--- Quick.java
+++ Quick.java
@@ -1,16 +1,20 @@
 import java.util.Random;
 public class Quick{
   public static void main(String[]args){
-
+    int[] ary = new int[]{999,999,999,4,1,0,3,2,999,999,999};
+    partition(ary, 0, ary.length);
+    System.out.println(intString(ary));
   }
 
   public static int partition(int[] data, int start, int end){
     Random r = new Random();
-    int pivIndex = r.nextInt(start - end) + start;
-    swap(data, 0, pivIndex);
-    for (int idx = start; idx < end; idx ++){
-      if (data[idx] > pivot) swap(data, idx, end - idx);
+    int pivIndex = r.nextInt(end - start) + start;
+    System.out.println(pivIndex + ": " + data[pivIndex]);
+    swap(data, start, pivIndex);
+    for (int idx = start + 1; idx < end; idx ++){
+      if (data[idx] > data[pivIndex]) swap(data, idx, end - idx);
     }
+    return -1;
   }
 
   public static void swap(int[] data, int idx1, int idx2){
@@ -24,7 +28,7 @@ public class Quick{
     for (int idx = 0; idx < ary.length; idx ++){
       output += ary[idx] + ", ";
     }
-    return output.substring(0, output.length() - 2);
+    return output.substring(0, output.length() - 2) + "]";
   }
 
 }
