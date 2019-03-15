60d2e4f77889f515b55cde3bf0f01c725a42e14e
sbrink00 <sbrink00@stuy.edu>
2019-03-15 06:58:54 -0400

    finished quick select but it slows down at larger numbers. Quicksort doesn't work when the max number is a low value

--- Quick.java
+++ Quick.java
@@ -7,41 +7,38 @@ public class Quick{
     //partition(ary, 0, ary.length - 1);
     //quicksort(ary, 0, ary.length - 1);
     //System.out.println(Arrays.toString(ary));
-    //int[] qs = new int[2000000];
-    //Random r = new Random();
-    //for (int idx = 0; idx < qs.length; idx ++) qs[idx] = r.nextInt(10000);
+    int[] qs = new int[10000];
+    Random r = new Random();
+    for (int idx = 0; idx < qs.length; idx ++) qs[idx] = r.nextInt(10000);
     //int[] as = qs;
     //quicksort(qs, 0, qs.length - 1);
     //Arrays.sort(as);
     //System.out.println(Arrays.equals(qs, as));
     int[] data = { 2, 10, 15, 23, 0,  5};
     System.out.println(quickselect(data , 0));//  would return 0
-    /*System.out.println(quickselect( ary , 1 ));//  would return 2
-    System.out.println(quickselect( ary , 2 ));//  would return 5
-    System.out.println(quickselect( ary , 3 ));//  would return 10
-    System.out.println(quickselect( ary , 4 ));//  would return 15
-    System.out.println(quickselect( ary , 5 ));//  would return 23*/
+    System.out.println(quickselect(data , 1 ));//  would return 2
+    System.out.println(quickselect(data, 2 ));//  would return 5
+    System.out.println(quickselect(qs, 34));//  would return 10
+    System.out.println(quickselect(data, 4 ));//  would return 15
+    System.out.println(quickselect(data, 5 ));//  would return 23
+  }
+
+  public static void quicksort(int[] data){
+    quicksort(data, 0, data.length - 1);
   }
 
   public static void quicksort(int[] ary, int lo, int hi){
-    //dont use zero as a parameter, use lo and high
     if (lo >= hi) return;
     int pivot = partition(ary, lo, hi);
     quicksort(ary, lo, pivot - 1);
     quicksort(ary, pivot + 1, hi);
   }
 
-  public static int quickselect(int []data, int k){
-    Random r = new Random();
-    int i = 0;
-    int j = data.length - 1;
-    int pivIndex = r.nextInt(j - i) + i;
+  public static int quickselect(int[] data, int k){
+    int pivIndex = partition(data, 0, data.length - 1);
     while (pivIndex != k){
-      pivIndex = partition(data, i, j);
-      if (pivIndex == k) return data[pivIndex];
-      if (pivIndex > k) j = pivIndex - 1;
-      if (pivIndex < k) i = pivIndex + 1;
-      System.out.println(Arrays.toString(data));
+      if (pivIndex > k) pivIndex = partition(data, 0, pivIndex);
+      if (pivIndex < k) pivIndex = partition(data, pivIndex, data.length - 1);;
     }
     return data[pivIndex];
   }
@@ -49,17 +46,19 @@ public class Quick{
   public static int partition(int[] data, int start, int end){
     if (data.length == 1) return 0;
     Random r = new Random();
-    int pivIndex = r.nextInt(end - start) + start;
+    int pivIndex = r.nextInt(end - start + 1) + start;
     int pivot = data[pivIndex];
     swap(data, start, pivIndex);
     int i = start + 1;
     int j = end;
+    int idx = 1;
     while (i != j){
       if (data[i] < pivot) i ++;
       else if (data[i] >= pivot){
         swap(data, i, j);
         j --;
       }
+      idx ++;
     }
     if (data[i] < pivot){
       swap(data, start, i);
